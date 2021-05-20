9fa5fd952919cac191acc768a074fa73ae2f5421
Farnaz <farnaz.adib.yaghmaie@liu.se>
2021-05-20 13:57:03 +0200

    Rerunning Jupyter notebook and saving data

--- fun_lib.py
+++ fun_lib.py
@@ -76,4 +76,56 @@ def plot_simple_car(trajectory, par):
     return ani
 
 
+def plot_and_save_simple_car(trajectory, par):
+    t = np.arange(0, par['t_stop'], par['dt'])  # create a time array from 0..t_stop sampled at 0.02 second steps
+    history_len = int(par['t_stop'] / par['dt'])  # how many trajectory points to display
+
+    x1 = trajectory[:, 0]
+    y1 = trajectory[:, 2]
+
+    fig = plt.figure(figsize=(5, 4))
+    gs = gridspec.GridSpec(2, 2)
+    ax1 = fig.add_subplot(autoscale_on=False, xlim=(-5.0, 5.0), ylim=(-5.0, 5.0))
+    ax1.set_aspect('equal')
+    ax1.grid()
+    ax1.set_xlabel('x')
+    ax1.set_ylabel('y')
+    ax1.annotate('Target',
+                xy=(0, 0), xycoords='data',
+                xytext=(35, 5), textcoords='offset points',
+                horizontalalignment='right', verticalalignment='bottom')
+
+    line, = ax1.plot(0, 0, '+', ms=15.0, mec='k')
+    line, = ax1.plot([], 'o-', lw=2, c='r')
+    trace, = ax1.plot([], ',-', lw=1, c='b')
+    time_template = 'time = %.1fs'
+    time_text = ax1.text(0.05, 0.9, '', transform=ax1.transAxes)
+    history_x, history_y, history_t = deque(maxlen=history_len), deque(maxlen=history_len), deque(maxlen=history_len)
+
+
+    def animate(i):
+        thisx = [x1[i]]
+        thisy = [y1[i]]
+        thist = i * par['dt']
+
+        if i == 0:
+            history_x.clear()
+            history_y.clear()
+            history_t.clear()
+
+        history_x.appendleft(thisx[0])
+        history_y.appendleft(thisy[0])
+        history_t.appendleft(thist)
+
+        line.set_data(thisx, thisy)
+        trace.set_data(history_x, history_y)
+        time_text.set_text(time_template % (i * par['dt']))
+        return line, trace, time_text
+
+    ani = animation.FuncAnimation(fig, animate, len(trajectory), interval=par['dt']*1000, blit=True)
+    writervideo = animation.FFMpegWriter(fps=60)
+    ani.save(par['path']+'\\simple_car.mp4', writer=writervideo)
+    return ani
+
+
 
